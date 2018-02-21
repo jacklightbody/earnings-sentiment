086e0e18439bbfadbdb98efd7f4f55a8a8e47424
Jack Lightbody <jack.lightbody@gmail.com>
2018-02-21 10:24:46 -0600

    Headless

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import re
-import requests
+from selenium import webdriver
 import datetime
 import os
 import time
@@ -11,15 +11,33 @@ def get_stock_calls(stock, min_year):
     path = "transcripts/"+stock
     if not os.path.isdir(path):
         os.mkdir(path)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=options)
     url = 'http://seekingalpha.com/symbol/'+stock+'/earnings/transcripts'
-    headers = {'User-Agent':'"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4)"'}
-    r = requests.get(url, headers = headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    driver.get(url)
+
+    body_el = driver.find_element_by_tag_name("body")
+
+    no_of_pagedowns = 50
+    # load everything in the infinite scroll
+    while no_of_pagedowns:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        no_of_pagedowns-=1
+
+    elem = driver.find_element_by_xpath("//*")
+    html = elem.get_attribute("outerHTML")
+    print(html)
+    soup = BeautifulSoup(html, 'html.parser')
     links = soup.find_all('a')
+    print(links)
+    print(len(links))
+
     for link in links:
         href = link.get('href')
         try:
-            if href and 'transcript' in href and 'article' in href:
+            if href and 'transcript' in href and 'earnings' in href:
                 transcript_url='http://seekingalpha.com/'+href+'?part=single'
                 date = link.find_next('div', {'class': 'date_on_by'}).text
                 date = date.split("•")[0]
@@ -37,11 +55,14 @@ def get_stock_calls(stock, min_year):
                         datetime_object = datetime.datetime.strptime(date+" "+str(cur_year), '%a, %b. %d %Y')
 
                 year = datetime_object.year
+                if year >= 2015: 
+                    continue
                 if year <= min_year:
-                    print(date)
                     return
-                call = requests.get(transcript_url, headers = headers)         
-                transcript_soup = BeautifulSoup(call.text, "html.parser")
+                driver.get(transcript_url)        
+                el = driver.find_element_by_xpath("//*")
+                page_html = el.get_attribute("outerHTML")
+                transcript_soup = BeautifulSoup(page_html, "html.parser")
                 try:
                     text = transcript_soup.find(id="a-cont").text
                     cleaned_text = text.split("Copyright policy:")[0]
@@ -53,6 +74,7 @@ def get_stock_calls(stock, min_year):
                     print("Transcript Parsing Error")
                     print(transcript_soup.text)
                     continue
+                print(href + " done")
 
 
         except TypeError as e:
@@ -63,7 +85,7 @@ def get_stock_calls(stock, min_year):
 
 
 if __name__ == "__main__":
-    stocks=['AAPL']#, 'GOOG', 'BA', 'CAT', 'KO', 'BUD', 'PEP']
+    stocks=['GOOG']#, 'BA', 'CAT', 'KO', 'BUD', 'PEP']
     min_year = 2012 
 
 
