098a8db39c2a0008f79e3985b6647cac61a2cd4f
Solomon Foster <solomonfoster@eduroam-173-52.dyn.carleton.edu>
2018-02-27 14:42:47 -0600

    quandl date method

--- quandlAPI.py
+++ quandlAPI.py
@@ -1,17 +1,31 @@
 import requests
 import json
 import pprint
+import datetime
 
 queryURL = "https://www.quandl.com/api/v3/datasets/WIKI/"
+APIkey = "jw6K62rHapnUJkLrCNC4"
+queryParameters = {'api_key': APIkey}
 
-def getCompanyStockData(symbol):
-    r = requests.get(queryURL + symbol + ".json")
+def getCompanyStockData(symbol, query_date):
+    r = requests.get(queryURL + symbol + ".json", params = queryParameters)
     j = json.loads(r.text)
-    return j
+    stockdata  = j['dataset']['data']
+
+    currentDate = datetime.date.today()
+    queryDate = datetime.date(int(query_date.split("-")[0]), int(query_date.split("-")[1]), int(query_date.split("-")[2]))
+    difference = int(((4/7) * (currentDate - queryDate)).days)
+
+    openPrice, closePrice, low, high = 0,0,0,0
+    for i in range(difference, len(stockdata)):
+        if stockdata[i][0] == query_date:
+            openPrice, closePrice, low, high = stockdata[i][1:5]
+
+    return [openPrice, closePrice, low, high]
 
 
 def main():
-    pprint.pprint(getCompanyStockData("AAPL"))
+    pprint.pprint(getCompanyStockData("AAPL", "2016-02-26"))
 
 if __name__ == "__main__":
     main()
